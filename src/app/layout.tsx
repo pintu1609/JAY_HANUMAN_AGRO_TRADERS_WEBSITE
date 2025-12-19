93a3a1e1a1b80c4835c733816eb16ff9abe72a85
Pintu <pintukumar808284@gmail.com>
2025-12-19 21:00:43 +0530

    implement whattapp chat

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "react-hot-toast";
+import WhatsAppButton from "@/components/whattapp/WhatsAppButton";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,7 +21,10 @@ export default function RootLayout({
       <body className={inter.className}>
       <Toaster position="top-right" />
 
-        {children}</body>
+        {children}
+        <WhatsAppButton />
+
+        </body>
     </html>
   );
 }
